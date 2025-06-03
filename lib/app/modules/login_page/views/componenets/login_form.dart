1d3bace1e2b19a497b8007ea904cc5ba6ad75d42
insabhi-aharijan <aharijan@insabhi.com>
2025-06-03 19:17:32 +0530

    massage page view

--- lib/app/modules/login_page/views/componenets/login_form.dart
+++ lib/app/modules/login_page/views/componenets/login_form.dart
@@ -6,177 +6,132 @@ import '../../../../common/app_fontsize.dart';
 import '../../../../routes/app_pages.dart';
 
 Center loginForm(dynamic controller) {
-    return Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AnimatedBuilder(
-                animation: controller.animation,
-                builder: (context, child) {
-                  return Transform.translate(
-                    offset: Offset(0, -controller.animation.value),
-                    child: child,
-                  );
-                },
-                child: SizedBox(
-                  height: 150,
-                  width: 150,
-                  child: Material(
-                    elevation: 20.0,
-                    shadowColor: AppColorList.MainShadow,
-                    borderRadius: BorderRadius.circular(20),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.asset('assets/images/app_icon.png'),
-                    ),
-                  ),
+  return Center(
+    child: SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+            animation: controller.animation,
+            builder: (context, child) {
+              return Transform.translate(
+                offset: Offset(0, -controller.animation.value),
+                child: child,
+              );
+            },
+            child: SizedBox(
+              height: 150,
+              width: 150,
+              child: Material(
+                elevation: 20.0,
+                shadowColor: AppColorList.MainShadow,
+                borderRadius: BorderRadius.circular(20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/images/app_icon.png'),
                 ),
               ),
-
-
-              const SizedBox(height: 10),
-
-              Text(
-                "Welcome to Agora Caller Mobile App",
-                style: TextStyle(
-                  fontSize: AppFontSize.size1,
-                  fontWeight: AppFontWeight.font2,
-                  color: AppColorList.AppText
-                ),
-                textAlign: TextAlign.center,
-              ),
-
-              const SizedBox(height: 40),
-
-              // Username field
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Username",
-                  style: TextStyle(
-                    fontWeight: AppFontWeight.font2,
-                    fontSize: AppFontSize.size3,
-                    color: AppColorList.AppText
-                  ),
-                ),
-              ),
-              const SizedBox(height: 6),
-              TextField(
-                controller: controller.UserName,
-                decoration: InputDecoration(
-                  labelText: 'Enter your username',
-                  labelStyle: TextStyle(
-                    color: AppColorList.AppText,
-                    fontSize: AppFontSize.size3,
-                    fontWeight: AppFontWeight.font1
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  
-                  // filled: true,
-                  // fillColor: AppColorList.AppTextField,
-                ),
-              ),
-
-              const SizedBox(height: 10),
-
-              // Password field
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Password",
-                  style: TextStyle(
-                    fontWeight: AppFontWeight.font2,
-                    fontSize: AppFontSize.size3,
-                    color: AppColorList.AppText
-                  ),
-                ),
-              ),
-              const SizedBox(height: 6),
-              TextField(
-                controller: controller.Password,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Enter your password',
-                  labelStyle: TextStyle(
-                    color: AppColorList.AppText,
-                    fontSize: AppFontSize.size3,
-                    fontWeight: AppFontWeight.font1
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  // filled: true,
-                  // fillColor: AppColorList.AppTextField,
-                ),
-              ),
-              const SizedBox(height: 10,),
-              // RememberMeCheckbox(controller: controller,),
-              // Padding(
-              //   padding: const EdgeInsets.all(10.0),
-              //   child: Row(
-              //     children: [
-              //       Container(
-              //         height: 20,
-              //         width: 20,
-              //         decoration: BoxDecoration(
-              //           color: Colors.white,
-              //           border: Border.all(),
-              //           borderRadius: BorderRadius.circular(8),
-              //         ),
-              //       ),
-              //       const SizedBox(width: 10,),
-              //       const Text('Remember me', style: TextStyle(fontSize: 16),),
-              //     ],
-              //   ),
-              // ),
-
-              const SizedBox(height: 40),
-
-              // Login button
-              Obx(() => controller.isLoading.value
-                ? const CircularProgressIndicator()
-                : SizedBox(
-                    width: double.infinity,
-                    height: 55,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(
-                          AppColorList.AppButtonColor,
-                        ),
-                        shadowColor: WidgetStateProperty.all(
-                          AppColorList.MainShadow, // Or any color you want for the shadow
-                        ),
-                        elevation: WidgetStateProperty.all(12)
-                      ),
-                      onPressed: () {
-                        // if (controller.UserName.text.isNotEmpty && controller.Password.text.isNotEmpty) {
-                        //   controller.login(
-                        //     controller.UserName.text.trim(), 
-                        //     controller.Password.text.trim().toString(),
-                        //     controller.rememberMe.value,
-                        //     );
-                        // } else {
-                        //   Get.snackbar("Error", "Please enter both username and password.");
-                        // }
-                        Get.offAllNamed(Routes.HOME);
-                      },
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          fontSize: AppFontSize.size1,
-                          fontWeight: AppFontWeight.font2,
-                          color: AppColorList.WhiteText,
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-            ],
+            ),
           ),
-        ),
-      );
+          const SizedBox(height: 10),
+          Text(
+            "Welcome to Agora Caller Mobile App",
+            style: TextStyle(
+              fontSize: AppFontSize.size1,
+              fontWeight: AppFontWeight.font2,
+              color: AppColorList.AppText
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 40),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Username",
+              style: TextStyle(
+                fontWeight: AppFontWeight.font2,
+                fontSize: AppFontSize.size3,
+                color: AppColorList.AppText
+              ),
+            ),
+          ),
+          const SizedBox(height: 6),
+          TextField(
+            controller: controller.UserName,
+            decoration: InputDecoration(
+              labelText: 'Enter your username',
+              labelStyle: TextStyle(
+                color: AppColorList.AppText,
+                fontSize: AppFontSize.size3,
+                fontWeight: AppFontWeight.font1
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Password",
+              style: TextStyle(
+                fontWeight: AppFontWeight.font2,
+                fontSize: AppFontSize.size3,
+                color: AppColorList.AppText
+              ),
+            ),
+          ),
+          const SizedBox(height: 6),
+          TextField(
+            controller: controller.Password,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Enter your password',
+              labelStyle: TextStyle(
+                color: AppColorList.AppText,
+                fontSize: AppFontSize.size3,
+                fontWeight: AppFontWeight.font1
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          const SizedBox(height: 40),
+          // Login button
+          Obx(() => controller.isLoading.value
+            ? const CircularProgressIndicator()
+            : SizedBox(
+                width: double.infinity,
+                height: 55,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(
+                      AppColorList.AppButtonColor,
+                    ),
+                    shadowColor: WidgetStateProperty.all(
+                      AppColorList.MainShadow, // Or any color you want for the shadow
+                    ),
+                    elevation: WidgetStateProperty.all(12)
+                  ),
+                  onPressed: () {
+                    Get.offAllNamed(Routes.HOME);
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: AppFontSize.size1,
+                      fontWeight: AppFontWeight.font2,
+                      color: AppColorList.WhiteText,
+                    ),
+                  ),
+                ),
+              ),
+            )
+        ],
+      ),
+    ),
+  );
 }
--- lib/app/modules/message_view/views/message_view_view.dart
+++ lib/app/modules/message_view/views/message_view_view.dart
@@ -1,7 +1,7 @@
+import 'package:agora_task/app/common/app_fontWeight.dart';
+import 'package:agora_task/app/common/app_fontsize.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/message_view_controller.dart';
 
 class MessageViewView extends GetView<MessageViewController> {
@@ -9,16 +9,120 @@ class MessageViewView extends GetView<MessageViewController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MessageViewView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'MessageViewView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      // appBar: AppBar(
+      //   title: const Text('Aditya Kumar Harijan',
+      //     style:TextStyle(
+      //       fontWeight: AppFontWeight.font3
+      //     )
+      //   ),
+      //   // centerTitle: true,
+      // ),
+      body: Column(
+        
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.6,
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      width: MediaQuery.of(context).size.width * 0.65,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.all(Radius.circular(100)),
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.black,
+                        )
+                      )
+                    ),
+                  ),
+                  SizedBox(
+            // height: 10
+            height: MediaQuery.of(context).size.height * 0.01,
+          ),
+
+                  Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Aditya Kumar Harijan',
+                style: TextStyle(
+                  fontSize: AppFontSize.size1,
+                  fontWeight: AppFontWeight.font3
+                ),
+                )
+            ],
+          ),
+                ],
+              ),
+            ],
+          ),
+          
+          
+          SizedBox(
+            height: MediaQuery.of(context).size.width * 0.3,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    width: MediaQuery.of(context).size.width * 0.23,
+                    decoration: BoxDecoration(
+                      color:Colors.black,
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border.all(
+                        width: 1,
+                      )
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Text(
+                    "Audio Call"
+                  )
+                ],
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.3,
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    width: MediaQuery.of(context).size.width * 0.23,
+                    decoration: BoxDecoration(
+                      color:Colors.black,
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border.all(
+                        width: 1,
+                      )
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Text(
+                    'Video Call'
+                  )
+
+                ],
+              )
+            ],
+          )
+        ],
+      )
+      
     );
   }
 }
